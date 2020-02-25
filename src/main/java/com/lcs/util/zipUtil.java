b906a31202ec8d6559b8b15163626dd5623965d1
lancs <570297931@qq.com>
2020-02-25 17:29:46 +0800

    zipUtil

--- src/main/java/com/lcs/util/zipUtil.java
+++ src/main/java/com/lcs/util/zipUtil.java
@@ -1,6 +1,11 @@
 package com.lcs.util;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
@@ -19,6 +24,8 @@ public final class zipUtil
 
     /**
      * 缓冲大小
+     * 10
+     * 100000000000
      */
     private static int BUFFERSIZE = 2 << 10;
 
@@ -178,14 +185,73 @@ public final class zipUtil
                 e.printStackTrace();
             }
         }
+
+
     }
+
+    /**
+     * 通过网络地址获取文件InputStream
+     *
+     * @param path 地址
+     * @return
+     */
+    public static InputStream returnBitMap(String path) {
+        URL url = null;
+        InputStream is = null;
+        try {
+            url = new URL(path);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();//利用HttpURLConnection对象,我们可以从网络中获取网页数据.
+            conn.setDoInput(true);
+            conn.connect();
+            is = conn.getInputStream();    //得到网络返回的输入流
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return is;
+    }
+
+
+
+    /**
+     * 通过网络url取文件，并保存
+     *
+     * @param path 文件保存路径
+     * @param url  网络地址
+     */
+    public static void uploadImage(String path, String url) {
+        try {
+            URL pathUrl = new URL(url);
+            DataInputStream dataInputStream = new DataInputStream(pathUrl.openStream());
+            File file = new File(path);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            while (dataInputStream.read(buffer) > 0) {
+                fileOutputStream.write(buffer);//将buffer中的字节写入文件中区
+            }
+            dataInputStream.close();//关闭输入流
+            fileOutputStream.close();//关闭输出流
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        zip(new String[] {"G:/Newifi Mini/路由器相关/自编译Newifi-mini固件/Newifi Mini"}, "g:/test.zip");
+        //zip(new String[] {"https://yunzhongbao.oss-cn-qingdao.aliyuncs.com/ceshi/3856.jpg?Expires=1614223469&OSSAccessKeyId=LTAIWLcvKrb5FOm7&Signature=ettVnFQ86Am8BzQCezbAncms6Nw%3D"}, "g:/test.zip");
         //unzip("g:/账号密码.zip", "d:/");
+        uploadImage("G:/hello.jpg","https://yunzhongbao.oss-cn-qingdao.aliyuncs.com/ceshi/3856.jpg?Expires=1614223469&OSSAccessKeyId=LTAIWLcvKrb5FOm7&Signature=ettVnFQ86Am8BzQCezbAncms6Nw%3D");
     }
 }
