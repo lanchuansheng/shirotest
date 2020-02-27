0cd99bfaaa0f793eb9ee4f05f9d8828c9cfe825a
lancs <570297931@qq.com>
2020-02-27 17:44:17 +0800

    2月27日zipUtil

--- src/main/java/com/lcs/controller/zipTestController.java
+++ src/main/java/com/lcs/controller/zipTestController.java
@@ -1,5 +1,18 @@
 package com.lcs.controller;
 
-public class zipTestController {
+import sun.net.ftp.impl.FtpClient;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class zipTestController {
+    public static void main(String[] args) {
+        SimpleDateFormat dfs = new SimpleDateFormat("yyyyMMddHHmmss");
+        String format = dfs.format(new Date());
+        Random r = new Random();
+        System.out.println(format+(r.nextInt(900000)+100000));
+
+
+    }
 }
